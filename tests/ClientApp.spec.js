af4b55334f0e645108113f803cc6466331d282ad
Oleh Martyniv <oleh.martyniv@gmail.com>
2023-10-17 09:18:50 +0300

    basic methods complete

---
+++ tests/ClientApp.spec.js
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+test.only('First practce', async function ({ page }) {
+  // open register page
+  await page.goto('https://rahulshettyacademy.com/client/');
+  await page.locator('.text-reset').click();
+  // register
+  const counter = 4;
+
+  await page.locator('#firstName').fill(`testF${counter}`);
+  await page.locator('#lastName').fill(`testL${counter}`);
+  await page.locator('#userEmail').fill(`test00${counter}@mail.com`);
+  await page.locator('#userMobile').fill(`100000000${counter}`);
+  await page.locator('#userPassword').fill(`P@ss000${counter}`);
+  await page.locator('#confirmPassword').fill(`P@ss000${counter}`);
+  await page.locator("[type='checkbox']").check();
+  await page.locator('#login').click();
+  // go to login
+  await expect(page.locator('h1.headcolor')).toBeAttached();
+  await expect(page.locator('h1.headcolor')).toHaveText(
+    'Account Created Successfully'
+  );
+  await page.locator("[routerlink='/auth']").click();
+  // login
+  await page.locator('#userEmail').fill(`test00${counter}@mail.com`);
+  await page.locator('#userPassword').fill(`P@ss000${counter}`);
+  await page.locator('#login').click();
+  // get first title
+  // await page.waitForLoadState('networkidle');
+  await page.locator('h5 > b').first().waitFor();
+  console.log(await page.locator('h5 > b').allTextContents());
+  await expect(page.locator('h5 > b').first()).toHaveText('zara coat 3');
+});
--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -3,11 +3,26 @@ import { test, expect } from '@playwright/test';
 test('Browser Context test', async function ({ browser }) {
   const context = await browser.newContext();
   const page = await context.newPage();
+  const userName = page.locator('#username');
+  const signIn = page.locator('#signInBtn');
+  const cardTitles = page.locator('.card-body a');
+
   await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
   console.log(await page.title());
-  await page.locator('#username').fill('rahulshetty');
-  await page.locator('#password').fill('learning');
-  await page.locator('#signInBtn').click();
+  await userName.fill('rahulshetty');
+  await page.locator("[type='password']").fill('learning');
+  await signIn.click();
+  console.log(await page.locator("[style*='none']").textContent());
+  await expect(page.locator("[style*='none']")).toContainText('Incorrect');
+
+  await userName.fill('');
+  await userName.fill('rahulshettyacademy');
+  await signIn.click();
+  console.log(await cardTitles.first().textContent());
+  console.log(await cardTitles.nth(1).textContent());
+
+  const allTitles = await cardTitles.allTextContents();
+  console.log(allTitles);
 });
 
 test('Page Playwright test', async function ({ page }) {
