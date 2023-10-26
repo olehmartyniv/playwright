ff5a11f4a1da9746bc92f087b48cbf01c15ed890
Oleh Martyniv <oleh.martyniv@gmail.com>
2023-10-26 23:59:25 +0300

    eslint config

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    ignores: ['node_modules/', 'playwright-report', 'test-results/'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      semi: ['error', 'always'],
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'prefer-const': 'error',
+    },
+  },
+];
